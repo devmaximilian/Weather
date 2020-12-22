ab708f1f9bfb0482426cfdf1e61fe1ea50874346
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2020-12-22 17:45:09 +0100

    Added test case class

---
+++ Tests/MetobsKitTests/ForecastPublisherTests.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maximilian Wendel on 2020-12-22.
+//
+
+import Foundation
+import MetobsKit
+import XCTest
+import Combine
+
+class ForecastPublisherTests: XCTestCase {
+    
+    func testForecastPublisher() {
+        
+    }
+    
+}
