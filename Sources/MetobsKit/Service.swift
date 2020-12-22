63258fea7db6a55077cd6d73e9c87f326b8b41f9
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2020-12-22 17:39:52 +0100

    Replaced ForecastService with ForecastPublisher

--- Sources/MetobsKit/Service.swift
+++ Sources/MetobsKit/Service.swift
@@ -22,123 +22,120 @@
 // SOFTWARE.
 //
 
+import class Foundation.HTTPURLResponse
+import class Foundation.URLResponse
+import class Foundation.JSONDecoder
+import class Foundation.URLSession
+import struct Foundation.URLError
+import struct Foundation.Data
+import struct Foundation.URL
+import protocol Combine.Publisher
+import protocol Combine.Subscriber
+import class Combine.PassthroughSubject
+import class Combine.AnyCancellable
+import struct Combine.AnyPublisher
+import enum Combine.Subscribers
+
+
+/// The service endpoint to send requests to
+@inline(__always)
+fileprivate let endpoint: String = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/{LONGITUDE}/lat/{LATITUDE}/data.json"
+
 /// A service that provides weather forecasts
-public class ForecastService: NSObject {
-    // MARK: Private properties
-
-    /// The service endpoint to send requests to
-    private var serviceEndpoint: String {
-        return "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/LONGITUDE/lat/LATITUDE/data.json"
-    }
-
-    /// A shared instance of forecast service
-    private static var sharedInstance: ForecastService?
-
-    // MARK: Private functions
-
-    /// Wrapps a network request as a `Promise<Value>`
-    private func request<Value: Decodable>(url: URL) -> Promise<Value> {
-        let promise = Promise<Value>()
-
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            // Make sure request did not result in an error
-            if let error = error {
-                return promise.reject(with: error)
-            }
-
-            // Make sure an acceptable response code is present on the response
-            if let response = response as? HTTPURLResponse {
-                if 400 ... 599 ~= response.statusCode {
-                    let error = URLError(.init(rawValue: response.statusCode))
-                    return promise.reject(with: error)
-                }
-            }
-
-            // Make sure the request returned data (and not an empty response)
-            guard let data = data else {
-                return promise.reject(with: URLError(.zeroByteResource))
-            }
-
-            // Attempt to decode JSON data from the response
-            do {
-                let value: Value = try data.decoded()
-                return promise.resolve(with: value)
-            } catch {
-                return promise.reject(with: error)
-            }
-        }.resume()
-
-        return promise
-    }
-
-    /// Constructs a `URL` to use when requesting forecasts
-    /// - Parameters:
-    ///   - latitude: The latitude to use for request
-    ///   - longitude: The longitude to use for request
-    private func buildURL(latitude: Double, longitude: Double) -> Promise<URL> {
-        // Remove decimals exceeding six positions as it will cause a 404 response
-        let values: (lat: Double, lon: Double) = (
-            lat: latitude.rounded(toPrecision: 6),
-            lon: longitude.rounded(toPrecision: 6)
-        )
-
-        let constructedURL = self.serviceEndpoint
-            .replacingOccurrences(of: "LONGITUDE", with: "\(values.lon)")
-            .replacingOccurrences(of: "LATITUDE", with: "\(values.lat)")
-
-        // Construct the URL
-        guard let url = URL(string: constructedURL) else {
-            return Promise<URL>(value: nil, error: URLError(.badURL))
-        }
-
-        return Promise<URL>(value: url)
-    }
-
-    // MARK: Public functions
-
-    /// Get the weather forecast `Observation` for a specific set of  coordinates
+public class ForecastPublisher: Publisher {
+    public typealias Output = Observation
+    public typealias Failure = Error
+    
+    private let subject: PassthroughSubject<Output, Failure> = .init()
+    private var cancellables: [String: AnyCancellable] = [:]
+    private let latitude: Double
+    private let longitude: Double
+    private var configured: Bool = false
+    
+    /// Get the weather forecast `Observation` for a specific set of coordinates
     /// - Note: See https://www.smhi.se/data/utforskaren-oppna-data/meteorologisk-prognosmodell-pmp3g-2-8-km-upplosning-api
     ///         for information about limitations (such as coordinate limitations)
     /// - Parameters:
     ///   - latitude: The coordinate latitude
     ///   - longitude: The coordinate longitude
-    /// - Returns: A `Promise` with the value `Observation`
-    public func get(latitude: Double, longitude: Double) -> Promise<Observation> {
-        let observationPromise: Promise<Observation> = Promise<Observation>()
-
-        // Request the forecast for the provided coordinates
-        let urlPromise = self.buildURL(latitude: latitude, longitude: longitude)
-
-        // Observe the request and forward the result
-        urlPromise.observe { urlResult in
-            switch urlResult {
-            case let .value(value):
-                let networkPromise: Promise<Observation> = self.request(url: value)
-                networkPromise.observe { networkResult in
-                    switch networkResult {
-                    case let .value(value):
-                        observationPromise.resolve(with: value)
-                    case let .error(error):
-                        observationPromise.reject(with: error)
-                    }
-                }
-            case let .error(error):
-                observationPromise.reject(with: error)
-            }
-        }
-
-        return observationPromise
+    public init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
     }
-
-    // MARK: Shared instance
-
-    /// A shared forecast-service instance
-    public static var shared: ForecastService {
-        guard let existingInstance = ForecastService.sharedInstance else {
-            let instance = ForecastService()
-            ForecastService.sharedInstance = instance
-            return instance
+    
+    /// Attaches the specified subscriber to this publisher.
+    ///
+    /// Implementations of ``Publisher`` must implement this method.
+    ///
+    /// The provided implementation of ``Publisher/subscribe(_:)``calls this method.
+    ///
+    /// - Parameter subscriber: The subscriber to attach to this ``Publisher``, after which it can receive values.
+    public func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+        self.subject.receive(subscriber: subscriber)
+        
+        guard self.configured == false else {
+            return
         }
-        return existingInstance
+        
+        self.configured = true
+        self.configure()
+    }
+}
+
+// MARK: - Private methods
+
+extension ForecastPublisher {
+    // Called upon first subscription
+    private func configure() {
+        let url = self.makeURL(latitude: latitude, longitude: longitude)
+        
+        self.cancellables["request"] = self.request(url)
+            .sink(receiveCompletion: { [weak self] completion in
+                guard let self = self, case .failure = completion else { return }
+                
+                self.subject.send(completion: completion)
+            }, receiveValue: { [weak self] input in
+                guard let self = self else { return }
+                
+                self.subject.send(input)
+            })
+    }
+    
+    private func request(_ url: URL) -> AnyPublisher<Output, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { (data: Data, response: URLResponse) -> (data: Data, response: HTTPURLResponse) in
+                guard let response = response as? HTTPURLResponse else {
+                    throw URLError(.badServerResponse)
+                }
+                // Check status code
+                guard 200...299 ~= response.statusCode else {
+                    throw URLError(.init(rawValue: response.statusCode))
+                }
+                return (data, response)
+            }
+            .map { (data: Data, response: URLResponse) -> Data in
+                return data
+            }
+            .decode(type: Output.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+    
+    private func makeURL(latitude: Double, longitude: Double) -> URL {
+        // Remove decimals exceeding six positions as it will cause a 404 response
+        let latitude = latitude.rounded(toPrecision: 6)
+        let longitude = longitude.rounded(toPrecision: 6)
+
+        let stringURL = endpoint
+            .replacingOccurrences(
+                of: "{LONGITUDE}",
+                with: longitude.description
+            )
+            .replacingOccurrences(
+                of: "{LATITUDE}",
+                with: latitude.description
+            )
+        
+        return URL(string: stringURL)
+            .unsafelyUnwrapped
     }
 }
